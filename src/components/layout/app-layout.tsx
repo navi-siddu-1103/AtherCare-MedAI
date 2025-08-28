7bd75a8f738052f25b6c71030e1a6e06acfe0f03
SM Siddalingaswamy <smsiddu2266@gmail.com>
2025-08-28 06:44:37 +0000

    Add functionality for emergency button, when click on it

--- src/components/layout/app-layout.tsx
+++ src/components/layout/app-layout.tsx
@@ -71,9 +71,11 @@ export default function AppLayout({ children }: AppLayoutProps) {
           </SidebarMenu>
         </SidebarContent>
         <SidebarFooter>
-          <Button variant="outline" className="w-full justify-start">
-            <HeartPulse className="mr-2 h-4 w-4" />
-            <span className="group-data-[collapsible=icon]:hidden">Emergency</span>
+          <Button variant="outline" className="w-full justify-start" asChild>
+            <Link href="/hospitals">
+              <HeartPulse className="mr-2 h-4 w-4" />
+              <span className="group-data-[collapsible=icon]:hidden">Emergency</span>
+            </Link>
           </Button>
         </SidebarFooter>
       </Sidebar>
