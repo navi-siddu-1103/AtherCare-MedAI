b5d1b3fcdf88f9573277d270bd1f9bfaf94a6362
SM Siddalingaswamy <smsiddu2266@gmail.com>
2025-08-28 06:45:37 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/layout/app-layout.tsx
+++ src/components/layout/app-layout.tsx
@@ -71,7 +71,7 @@ export default function AppLayout({ children }: AppLayoutProps) {
           </SidebarMenu>
         </SidebarContent>
         <SidebarFooter>
-          <Link href="/hospitals" legacyBehavior passHref>
+          <Link href="/hospitals">
             <Button variant="outline" className="w-full justify-start">
               <HeartPulse className="mr-2 h-4 w-4" />
               <span className="group-data-[collapsible=icon]:hidden">Emergency</span>
